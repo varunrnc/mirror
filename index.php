c83d777a2c00fd6cce491475d4fbb7f932b00c65
varunrnc <barunpandit143@gmail.com>
2025-03-05 23:48:20 +0530

    update

--- index.php
+++ index.php
@@ -9,5 +9,6 @@
     <h1>Hello, World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque ea tenetur labore excepturi debitis ullam dolorem nostrum corporis dolorum eius pariatur commodi nesciunt beatae optio inventore repellat ipsam nisi ducimus quibusdam repudiandae nobis nemo rerum, dicta omnis. Repellendus corrupti iure quibusdam nostrum doloribus voluptate facere fugiat quia provident ipsa maxime similique, officia, ratione, esse unde reiciendis magnam ullam sunt quasi odio praesentium explicabo! Rem necessitatibus magnam praesentium culpa velit dignissimos sapiente omnis? Optio, amet repudiandae tempore odio ea accusantium voluptatum assumenda veniam magnam ipsam distinctio adipisci quaerat quia, iusto magni neque perspiciatis dolor dolorem quisquam possimus? Cumque ratione fuga et.</p>
     <button>Read More</button>
+    <h3>Testing</h3>
 </body>
 </html>
